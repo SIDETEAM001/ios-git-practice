ad61ec812c358fd7ad16cbc1bed0a2e3319bbb8b
kms0524 <gcong2011@gmail.com>
2023-11-08 20:32:18 +0900

    initial

---
+++ gitpractice/gitpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitpractice
+//
+//  Created by 강민성 on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
