5c4faf12bedf73997cb518bb2ac432097ee900a9
GOGO <145358595+dumok-nim@users.noreply.github.com>
2023-11-09 12:06:39 +0900

    Update ViewController.swift

--- gitpractice/gitpractice/ViewController.swift
+++ gitpractice/gitpractice/ViewController.swift
@@ -14,15 +14,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    //22
-    //1
-    //33
-    //55
-    //44
-    //66
-
-    // test3
-    // test4
-    // test6
+    /* write some test message */
 }
 
